5721556e2c0efa764e8bf971fa6cad1a9192bb36
ai2525desu <umi08062525@gmail.com>
2025-11-10 22:42:31 +0900

    勤怠登録画面作成開始

--- src/resources/views/user/attendance/registration.blade.php
+++ src/resources/views/user/attendance/registration.blade.php
@@ -5,5 +5,28 @@
 @endsection
 
 @section('content')
-出勤登録画面
+<div class="attendance-registration__content">
+    <div class="attendance-registration__item">
+        <div class="attendance-registration__item--working-status">
+            <span class="working-status">勤務外</span>
+        </div>
+        <div class="attendance-registration__item--working-day">
+            <!-- コントローラーでCarbonを使用して取得かな？ -->
+            <p class="working-day">xxxx年x月x日</p>
+        </div>
+        <div class="attendance-registration__item--current-time">
+            <p class="current-time">00:00（現在時刻表示）</p>
+        </div>
+    </div>
+    <div class="attendance-registration__form">
+        <form class="" action="" method="post" novalidate>
+            @csrf
+            <div class="attendance-registration__form-button">
+                <button class="attendance-registration__form-button--submit" type="submit">
+                    出勤
+                </button>
+            </div>
+        </form>
+    </div>
+</div>
 @endsection
