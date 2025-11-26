003594506856a215f568dc676a54de4dba78ac14
ai2525desu <umi08062525@gmail.com>
2025-11-27 00:34:52 +0900

    詳細画面のレイアウトとレスポンシブOK

--- src/resources/views/user/attendance/detail.blade.php
+++ src/resources/views/user/attendance/detail.blade.php
@@ -9,7 +9,6 @@
     <h1 class="user-attendance-detail__title">
         勤怠詳細
     </h1>
-    <!-- input type="time"の件で検討 -->
     <form class="user-attendance-detail__correction-form" method="post" action="">
         <input type="hidden" name="attendance_id" value="{{ $attendance->id }}">
         <input type="hidden" name="user_id" value="{{ $user->id }}">
@@ -18,8 +17,8 @@
             <tr class="correction-form__line">
                 <th class="correction-form__heading">名前</th>
                 <td class="correction-form__item">
-                    <div class="correction-form__item--date">
-                        <span class="correction-form__item--text">
+                    <div class="correction-form__item--information">
+                        <span class="correction-form__text right">
                             {{ $user->name }}
                         </span>
                     </div>
@@ -28,11 +27,11 @@
             <tr class="correction-form__line">
                 <th class="correction-form__heading">日付</th>
                 <td class="correction-form__item">
-                    <div class="correction-form__item--date">
-                        <span class="correction-form__item--text">
+                    <div class="correction-form__item--information">
+                        <span class="correction-form__text right">
                             {{ $attendance->work_date->format('Y年') }}
                         </span>
-                        <span class="correction-form__item--text">
+                        <span class="correction-form__text left">
                             {{ $attendance->work_date->format('m月d日') }}
                         </span>
                     </div>
@@ -41,32 +40,76 @@
             <tr class="correction-form__line">
                 <th class="correction-form__heading">出勤・退勤</th>
                 <td class="correction-form__item">
-                    <div class="correction-form__item--date">
-                        <input class="correction-form__item--input" type="datetime" name="correct_clock_in" value="{{ old('correct_clock_in',$attendance?->clock_in->format('H:i')) }}">
-                        <span class="correction-form__item--text">~</span>
-                        <input class="correction-form__item--input" type="datetime" name="correct_clock_out" value="{{ old('correct_clock_out', $attendance?->clock_out->format('H:i')) }}">
+                    <div class="correction-form__item--information">
+                        <input class="correction-form__input right" type="text" name="correct_clock_in" value="{{ old('correct_clock_in',$attendance?->clock_in->format('H:i')) }}">
+                        <span class="correction-form__text middle">~</span>
+                        <input class="correction-form__input left" type="text" name="correct_clock_out" value="{{ old('correct_clock_out', $attendance?->clock_out->format('H:i')) }}">
                     </div>
                 </td>
             </tr>
+            @if ($attendance->attendancebreaks->count() >= 2)
             @foreach ($attendance->attendanceBreaks as $index => $break)
             <tr class="correction-form__line">
-                <th class="correction-form__heading">休憩{{ $index + 1 }}</th>
+                <th class="correction-form__heading">休憩{{ $loop->first ? '' : $loop->iteration }}</th>
                 <td class="correction-form__item">
-                    <div class="correction-form__item--date">
-                        <input class="correction-form__item--input" type="datetime" name="correct_break_start[{{ $index }}][start]" value="{{ old('correct_break_start', $break?->break_start->format('H:i')) }}">
-                        <span class="correction-form__item--text">~</span>
-                        <input class="correction-form__item--input" type="datetime" name="correct_break_end[{{ $index }}][end]" value="{{ old('correct_break_end', $break?->break_end->format('H:i')) }}">
+                    <div class="correction-form__item--information">
+                        <input class="correction-form__input right" type="text" name="correct_break_start[{{ $index }}][start]" value="{{ old('correct_break_start.$index.start', $break?->break_start->format('H:i')) }}">
+                        <span class="correction-form__text middle">~</span>
+                        <input class="correction-form__input left" type="text" name="correct_break_end[{{ $index }}][end]" value="{{ old('correct_break_end.$index.end', $break?->break_end->format('H:i')) }}">
                     </div>
                 </td>
             </tr>
             @endforeach
+            @elseif ($attendance->attendancebreaks->count() === 1)
+            <tr class="correction-form__line">
+                <th class="correction-form__heading">休憩</th>
+                <td class="correction-form__item">
+                    <div class="correction-form__item--information">
+                        <input class="correction-form__input right" type="text" name="correct_break_start[0][start]" value="{{ old('correct_break_start.0.start', $attendance->attendanceBreaks[0]?->break_start->format('H:i')) }}">
+                        <span class="correction-form__text middle">~</span>
+                        <input class="correction-form__input left" type="text" name="correct_break_end[0][end]" value="{{ old('correct_break_end.0.end', $attendance->attendanceBreaks[0]?->break_end->format('H:i')) }}">
+                    </div>
+                </td>
+            </tr>
+            <tr class="correction-form__line">
+                <th class="correction-form__heading">休憩2</th>
+                <td class="correction-form__item">
+                    <div class="correction-form__item--information">
+                        <input class="correction-form__input right" type="text" name="correct_break_start[1][start]" value="{{ old('correct_break_start.1.start') }}">
+                        <span class="correction-form__text middle">~</span>
+                        <input class="correction-form__input left" type="text" name="correct_break_end[1][end]" value="{{ old('correct_break_end.1.end') }}">
+                    </div>
+                </td>
+            </tr>
+            @else
+            <tr class="correction-form__line">
+                <th class="correction-form__heading">休憩</th>
+                <td class="correction-form__item">
+                    <div class="correction-form__item--information">
+                        <input class="correction-form__input right" type="text" name="correct_break_start[0][start]" value="{{ old('correct_break_start.0.start') }}">
+                        <span class="correction-form__text middle">~</span>
+                        <input class="correction-form__input left" type="text" name="correct_break_end[0][end]" value="{{ old('correct_break_end.0.end') }}">
+                    </div>
+                </td>
+            </tr>
+            <tr class="correction-form__line">
+                <th class="correction-form__heading">休憩2</th>
+                <td class="correction-form__item">
+                    <div class="correction-form__item--information">
+                        <input class="correction-form__input" type="text" name="correct_break_start[1][start]" value="{{ old('correct_break_start.1.start') }}">
+                        <span class="correction-form__text">~</span>
+                        <input class="correction-form__input" type="text" name="correct_break_end[1][end]" value="{{ old('correct_break_end.1.end') }}">
+                    </div>
+                </td>
+            </tr>
+            @endif
             <tr class="correction-form__line">
                 <th class="correction-form__heading">
                     備考
                 </th>
                 <td class="correction-form__item">
-                    <div class="correction-form__item--date">
-                        <textarea class="correction-form__item--textarea" name="remarks">{{ old('remarks') }}</textarea>
+                    <div class="correction-form__item--remark">
+                        <textarea class="correction-form__textarea" name="remarks">{{ old('remarks') }}</textarea>
                     </div>
                 </td>
             </tr>
