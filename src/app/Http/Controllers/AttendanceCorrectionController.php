0395b9e05d6461172ff78ce6c6a3eba042aaa614
ai2525desu <umi08062525@gmail.com>
2025-12-03 00:33:29 +0900

    一般ユーザー申請一覧簡易ビュー作成

--- src/app/Http/Controllers/AttendanceCorrectionController.php
+++ src/app/Http/Controllers/AttendanceCorrectionController.php
@@ -2,14 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\AttendanceCorrectRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AttendanceCorrectionController extends Controller
 {
     // 一般ユーザーの申請一覧画面表示
-    public function indexCorrection()
+    public function indexCorrection(Request $request)
     {
-        return view('user.stamp_correction_request.list');
+        // tab は動的セグメント＝画面のリロードで切り替えするため記述注意
+        $tab = $request->query('tab', 'pending');
+        $user = Auth::user();
+        $attendanceCorrection = AttendanceCorrectRequest::with('attendanceBreakCorrects');
+        return view('user.stamp_correction_request.list', compact('tab', 'user',));
     }
 
     // 管理者の申請一覧画面表示
--- src/resources/views/user/stamp_correction_request/list.blade.php
+++ src/resources/views/user/stamp_correction_request/list.blade.php
@@ -5,5 +5,163 @@
 @endsection
 
 @section('content')
-一般ユーザーの申請一覧画面
+@if (session('message'))
+<div class="user-corection-list__session-message">
+    {{ session('message') }}
+    セッションメッセージ
+</div>
+@endif
+<div class="user-corection-list__content">
+    <h1 class="user-corection-list__title">
+        申請一覧
+    </h1>
+    <div class="user-corection-list__tab">
+        <div class="correction-tab__header">
+            <li class="correction-tab__item">
+                <a class="item__pending-list {{ $tab === 'pending' ? 'is-active' : '' }}" href="{{ route('user.stamp_correction_request.list', ['tab' => 'pending']) }}">承認待ち</a>
+            </li>
+            <li class="correction-tab__item">
+                <a class="item__approved-list {{ $tab === 'approved' ? 'is-active' : '' }}" href="{{ route('user.stamp_correction_request.list', ['tab' => 'approved']) }}">承認済み</a>
+            </li>
+        </div>
+        <div class="correction-tab__body">
+            <table class="correction-tab__table {{ $tab === 'pending' ? 'is-active' : '' }}">
+                <thead>
+                    <tr class="correction-tab__line">
+                        <th class="correction-tab__header">
+                            <span class="correction-tab__heading">
+                                状態
+                            </span>
+                        </th>
+                        <th class="correction-tab__header">
+                            <span class="correction-tab__heading">
+                                名前
+                            </span>
+                        </th>
+                        <th class="correction-tab__header">
+                            <span class="correction-tab__heading">
+                                対象日時
+                            </span>
+                        </th>
+                        <th class="correction-tab__header">
+                            <span class="correction-tab__heading">
+                                申請理由
+                            </span>
+                        </th>
+                        <th class="correction-tab__header">
+                            <span class="correction-tab__heading">
+                                申請日時
+                            </span>
+                        </th>
+                        <th class="correction-tab__header">
+                            <span class="correction-tab__heading">
+                                詳細
+                            </span>
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {{--@foreach ()--}}
+                    <tr class="correction-tab__line">
+                        <td class="correction-tab__information">
+                            <!-- status -->
+                            承認待ち
+                        </td>
+                        <td class="correction-tab__information">
+                            <!-- user->name -->
+                            ユーザー名
+                        </td>
+                        <td class="correction-tab__information">
+                            <!-- work_date -->
+                            2000/00/00
+                        </td>
+                        <td class="correction-tab__information">
+                            <!-- remarks -->
+                            申請の理由
+                        </td>
+                        <td class="correction-tab__information">
+                            <!-- request_date -->
+                            申請した日時
+                        </td>
+                        <td class="correction-tab__information">
+                            <!-- 詳細画面への遷移 -->
+                            <a class="correction-tab__screen-transition" href="{{ route('user.attendance.detail', ['id' => $day['attendance']->id]) }}">
+                                詳細
+                            </a>
+                        </td>
+                    </tr>
+                    {{--@endforeach--}}
+                </tbody>
+            </table>
+            <table class="correction-tab__table {{ $tab === 'approved' ? 'is-active' : '' }}">
+                <thead>
+                    <tr class="correction-tab__line">
+                        <th class="correction-tab__header">
+                            <span class="correction-tab__heading">
+                                状態
+                            </span>
+                        </th>
+                        <th class="correction-tab__header">
+                            <span class="correction-tab__heading">
+                                名前
+                            </span>
+                        </th>
+                        <th class="correction-tab__header">
+                            <span class="correction-tab__heading">
+                                対象日時
+                            </span>
+                        </th>
+                        <th class="correction-tab__header">
+                            <span class="correction-tab__heading">
+                                申請理由
+                            </span>
+                        </th>
+                        <th class="correction-tab__header">
+                            <span class="correction-tab__heading">
+                                申請日時
+                            </span>
+                        </th>
+                        <th class="correction-tab__header">
+                            <span class="correction-tab__heading">
+                                詳細
+                            </span>
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {{--@foreach ()--}}
+                    <tr class="correction-tab__line">
+                        <td class="correction-tab__information">
+                            <!-- status -->
+                            承認待ち
+                        </td>
+                        <td class="correction-tab__information">
+                            <!-- user->name -->
+                            ユーザー名
+                        </td>
+                        <td class="correction-tab__information">
+                            <!-- work_date -->
+                            2000/00/00
+                        </td>
+                        <td class="correction-tab__information">
+                            <!-- remarks -->
+                            申請の理由
+                        </td>
+                        <td class="correction-tab__information">
+                            <!-- request_date -->
+                            申請した日時
+                        </td>
+                        <td class="correction-tab__information">
+                            <!-- 詳細画面への遷移 -->
+                            <a class="correction-tab__screen-transition" href="{{ route('user.attendance.detail', ['id' => $day['attendance']->id]) }}">
+                                詳細
+                            </a>
+                        </td>
+                    </tr>
+                    {{--@endforeach--}}
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
 @endsection
