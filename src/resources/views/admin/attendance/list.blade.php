a8afffad00ea8ea2bf45f14761c5bd2ff43e2520
ai2525desu <umi08062525@gmail.com>
2025-12-16 22:19:22 +0900

    admin/user勤怠一覧画面CSS＋レスポンシブOK

--- src/resources/views/admin/attendance/list.blade.php
+++ src/resources/views/admin/attendance/list.blade.php
@@ -6,10 +6,10 @@
 
 @section('content')
 <div class="admin-attendance-list__content">
-    <h1 class="user-attendance-list__title">
+    <h1 class="admin-attendance-list__title">
         {{ $targetDate->isoFormat('Y年M月D日') }}の勤怠
     </h1>
-    <div class="user-attendance-list__pagination">
+    <div class="admin-attendance-list__pagination">
         <a class="pagination__day" href="{{ route('admin.attendance.list', ['date' => $previous]) }}">
             <img class="pagination__day--arrow" src="{{ asset('storage/arrow.svg') }}" alt="←">
             <span class="pagination__day--title">
@@ -28,47 +28,48 @@
             </span>
             <img class="pagination__day--arrow right-direction" src="{{ asset('storage/arrow.svg') }}" alt="→">
         </a>
-        <div class="admin-attendance-list__day-attendance">
-            <table class="day-attendance__table">
-                <thead>
-                    <tr class="day-attendance__table-header">
-                        <th class="day-attendance__heading">名前</th>
-                        <th class="day-attendance__heading">出勤</th>
-                        <th class="day-attendance__heading">退勤</th>
-                        <th class="day-attendance__heading">休憩</th>
-                        <th class="day-attendance__heading">合計</th>
-                        <th class="day-attendance__heading">詳細</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($attendances as $attendance)
-                    <tr class="day-attendance__item-line">
-                        <td class="day-attendance__item">
-                            {{ $attendance->user->name }}
-                        </td>
-                        <td class="day-attendance__item">
-                            {{ $attendance->clock_in->format('H:i') }}
-                        </td>
-                        <td class="day-attendance__item">
-                            {{ $attendance->clock_out->format('H:i') }}
-                        </td>
-                        <td class="day-attendance__item">
-                            {{ $attendance->displayBreakTimeInHourFormat() }}
-                        </td>
-                        <td class="day-attendance__item">
-                            {{ $attendance->displayWorkingTimeInHourFormat() }}
-                        </td>
-                        <td class="day-attendance__item">
-                            <!-- href部分はルートなど確率してから記述すること -->
-                            {{-- href="{{ route('admin.attendance.detail', ['id' => $attendance->id]) }}" --}}
-                            <a class="day-attendance__screen-transition" href="">
-                                詳細
-                            </a>
-                        </td>
-                    </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </div>
     </div>
-    @endsection
+    <div class="admin-attendance-list__day-attendance">
+        <table class="day-attendance__table">
+            <thead>
+                <tr class="day-attendance__table-header">
+                    <th class="day-attendance__heading">名前</th>
+                    <th class="day-attendance__heading">出勤</th>
+                    <th class="day-attendance__heading">退勤</th>
+                    <th class="day-attendance__heading">休憩</th>
+                    <th class="day-attendance__heading">合計</th>
+                    <th class="day-attendance__heading">詳細</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($attendances as $attendance)
+                <tr class="day-attendance__item-line">
+                    <td class="day-attendance__item">
+                        {{ $attendance->user->name }}
+                    </td>
+                    <td class="day-attendance__item">
+                        {{ $attendance->clock_in->format('H:i') }}
+                    </td>
+                    <td class="day-attendance__item">
+                        {{ $attendance->clock_out->format('H:i') }}
+                    </td>
+                    <td class="day-attendance__item">
+                        {{ $attendance->displayBreakTimeInHourFormat() }}
+                    </td>
+                    <td class="day-attendance__item">
+                        {{ $attendance->displayWorkingTimeInHourFormat() }}
+                    </td>
+                    <td class="day-attendance__item">
+                        <!-- href部分はルートなど確率してから記述すること -->
+                        {{-- href="{{ route('admin.attendance.detail', ['id' => $attendance->id]) }}" --}}
+                        <a class="day-attendance__screen-transition" href="">
+                            詳細
+                        </a>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</div>
+@endsection
