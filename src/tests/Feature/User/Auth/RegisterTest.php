2d0dedcabb47d9077b2d4626af76a2782c8e1c70
ai2525desu <umi08062525@gmail.com>
2026-01-04 00:30:45 +0900

    RegisterTest不要use削除

--- src/tests/Feature/User/Auth/RegisterTest.php
+++ src/tests/Feature/User/Auth/RegisterTest.php
@@ -3,7 +3,6 @@
 namespace Tests\Feature\User\Auth;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Support\Facades\Hash;
 use Tests\TestCase;
 
 class RegisterTest extends TestCase
