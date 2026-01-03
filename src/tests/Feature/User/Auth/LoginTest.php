743c031810cb8f449871fb1b8d33c5af4b77e229
ai2525desu <umi08062525@gmail.com>
2026-01-04 00:29:43 +0900

    LoginTest記述＋pass完了

--- src/tests/Feature/User/Auth/LoginTest.php
+++ src/tests/Feature/User/Auth/LoginTest.php
@@ -2,21 +2,73 @@
 
 namespace Tests\Feature\User\Auth;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Hash;
 use Tests\TestCase;
 
 class LoginTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example()
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
+    // ログインテストで使用する一般ユーザーの登録
+    private function createBaseUser(array $override = [])
+    {
+        return User::factory()->create(array_merge([
+            'name' => 'ログインテストユーザー',
+            'email' => 'login-test@example.co.jp',
+            'password' => Hash::make('password'),
+        ], $override));
+    }
+
+    // メールアドレス未入力のバリデーションチェック
+    public function test_login_email_not_entered_validation_error()
+    {
+        $this->createBaseUser();
+
+        $response = $this->get('/login');
         $response->assertStatus(200);
+
+        $response = $this->post('/login', [
+            'email' => '',
+            'password' => 'password',
+        ]);
+
+        $response->assertSessionHasErrors(['email']);
+        $errors = session('errors')->getBag('default');
+        $this->assertEquals('メールアドレスを入力してください', $errors->first('email'));
+    }
+
+    // パスワードが未入力のバリデーションチェック
+    public function test_login_password_not_entered_validation_error()
+    {
+        $this->createBaseUser();
+
+        $response = $this->get('/login');
+        $response->assertStatus(200);
+
+        $response = $this->post('/login', [
+            'email' => 'login-test@example.co.jp',
+            'password' => '',
+        ]);
+
+        $response->assertSessionHasErrors(['password']);
+        $errors = session('errors')->getBag('default');
+        $this->assertEquals('パスワードを入力してください', $errors->first('password'));
+    }
+
+    public function test_login_information_mismatch_error()
+    {
+        $this->createBaseUser();
+
+        $response = $this->get('/login');
+        $response->assertStatus(200);
+
+        $response = $this->post('/login', [
+            'email' => 'wrong@example.co.jp',
+            'password' => 'password',
+        ]);
+
+        $response->assertSessionHas('errorMessage', 'ログイン情報が登録されていません');
     }
 }
