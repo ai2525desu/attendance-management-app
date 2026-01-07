2e33fbec41f2bc325226f3e011fe1d4c1d3f6bfe
ai2525desu <umi08062525@gmail.com>
2026-01-07 13:08:03 +0900

    CheckAttendanceStatusTest共通事項をsetUp化

--- src/tests/Feature/User/Attendance/CheckAttendanceStatusTest.php
+++ src/tests/Feature/User/Attendance/CheckAttendanceStatusTest.php
@@ -4,15 +4,23 @@ namespace Tests\Feature\User\Attendance;
 
 use App\Models\Attendance;
 use App\Models\AttendanceBreak;
+use App\Models\User;
 use Carbon\Carbon;
-use League\Flysystem\FileNotFoundException;
-use Symfony\Component\HttpKernel\DependencyInjection\ConfigurableExtension;
 use Tests\UserTestCase;
-
-use function PHPUnit\Framework\assertNull;
 
 class CheckAttendanceStatusTest extends UserTestCase
 {
+    protected User $user;
+    protected Carbon $now;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        Carbon::setTestNow(Carbon::create(2026, 1, 6, 8, 0, 0));
+        $this->now = Carbon::now();
+        $this->user = $this->loginUser();
+    }
+
     protected function tearDown(): void
     {
         Carbon::setTestNow();
@@ -22,9 +30,6 @@ class CheckAttendanceStatusTest extends UserTestCase
     // 勤務外と表示されているか
     public function test_to_check_if_your_status_is_off_duty()
     {
-        Carbon::setTestNow(Carbon::create(2026, 1, 6, 8, 0, 0));
-        $this->loginUser();
-
         $response = $this->get(route('user.attendance.registration'));
         $response->assertStatus(200);
         $response->assertSee('勤務外');
@@ -33,13 +38,10 @@ class CheckAttendanceStatusTest extends UserTestCase
     // 出勤中と表示されているか
     public function test_to_check_if_your_status_is_at_work()
     {
-        Carbon::setTestNow(Carbon::create(2026, 1, 7, 8, 0, 0));
-        $dateTime = Carbon::now();
-        $user = $this->loginUser();
         Attendance::create([
-            'user_id' => $user->id,
-            'work_date' => $dateTime->toDateString(),
-            'clock_in' => $dateTime->toDateTimeString(),
+            'user_id' => $this->user->id,
+            'work_date' => $this->now->toDateString(),
+            'clock_in' => $this->now->toDateTimeString(),
         ]);
 
         $response = $this->get(route('user.attendance.registration'));
@@ -50,21 +52,12 @@ class CheckAttendanceStatusTest extends UserTestCase
     // 休憩中と表示されているどうか
     public function test_to_check_if_your_status_is_on_break()
     {
-        Carbon::setTestNow(Carbon::create(2026, 1, 7, 8, 0, 0));
-        $dateTime = Carbon::now();
-        $user = $this->loginUser();
-
-        Attendance::create([
-            'user_id' => $user->id,
-            'work_date' => $dateTime->toDateString(),
-            'clock_in' => $dateTime->toDateTimeString(),
+        $attendance = Attendance::create([
+            'user_id' => $this->user->id,
+            'work_date' => $this->now->toDateString(),
+            'clock_in' => $this->now->toDateTimeString(),
         ]);
 
-        $attendance = Attendance::where('user_id', $user->id)
-            ->whereDate('work_date', Carbon::today())
-            ->whereNotNull('clock_in')
-            ->whereNull('clock_out')
-            ->first();
         AttendanceBreak::create([
             'attendance_id' => $attendance->id,
             'break_start' => Carbon::parse($attendance->clock_in)->addHours(4)->toDateTimeString(),
@@ -78,15 +71,11 @@ class CheckAttendanceStatusTest extends UserTestCase
     // 退勤済と表示されているかどうか
     public function test_to_check_if_status_is_clocked_out()
     {
-        Carbon::setTestNow(Carbon::create(2026, 1, 7, 8, 0, 0));
-        $dateTime = Carbon::now();
-        $user = $this->loginUser();
-
         Attendance::create([
-            'user_id' => $user->id,
-            'work_date' => $dateTime->toDateString(),
-            'clock_in' => $dateTime->toDateTimeString(),
-            'clock_out' => Carbon::parse($dateTime)->addHour(8)->toDateTimeString(),
+            'user_id' => $this->user->id,
+            'work_date' => $this->now->toDateString(),
+            'clock_in' => $this->now->toDateTimeString(),
+            'clock_out' => Carbon::parse($this->now)->addHour(8)->toDateTimeString(),
         ]);
 
         $response = $this->get(route('user.attendance.registration'));
