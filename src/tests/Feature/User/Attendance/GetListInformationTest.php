2f5b1aaf92e4547a914b0bd6d32451eb114f35a5
ai2525desu <umi08062525@gmail.com>
2026-01-11 22:47:08 +0900

    一覧データ取得テスト記述途中「翌月」

--- src/tests/Feature/User/Attendance/GetListInformationTest.php
+++ src/tests/Feature/User/Attendance/GetListInformationTest.php
@@ -8,6 +8,8 @@ use App\Models\User;
 use Carbon\Carbon;
 use Tests\UserTestCase;
 
+use function PHPUnit\Framework\assertDoesNotMatchRegularExpression;
+
 class GetListInformationTest extends UserTestCase
 {
     protected User $user;
@@ -61,5 +63,84 @@ class GetListInformationTest extends UserTestCase
         foreach ($attendances as $attendance) {
             $response->assertSee('01:00');
         }
+    }
+
+    // 勤怠一覧画面に遷移した際に現在の月が表示されているか
+    public function test_whether_the_current_month_is_displayed_when_transitioning_to_the_attendance_list_screen()
+    {
+        Carbon::setTestNow(Carbon::create(2026, 1, 6, 8, 0, 0));
+        $currentMonth = Carbon::now()->format('Y/m');
+
+        $response = $this->get(route('user.attendance.list'));
+        $response->assertStatus(200);
+        $response->assertSee($currentMonth);
+    }
+
+    // 前月の勤怠データが表示されるか
+    public function test_whether_the_previous_month_attendance_is_displayed()
+    {
+        Carbon::setTestNow(Carbon::create(2026, 1, 6, 8, 0, 0));
+
+        $previousMonthData = Carbon::now()->subMonth();
+        $previousAttendance = Attendance::factory()->worked($previousMonthData)->for($this->user)->create();
+
+        $clockInTime = Carbon::parse($previousAttendance->clock_in);
+        $breakStart = $clockInTime->copy()->addHour(4);
+        $breakEnd = $breakStart->copy()->addHour(1);
+        AttendanceBreak::factory()
+            ->forAttendance($previousAttendance)
+            ->create([
+                'break_start' => $breakStart->toDateTimeString(),
+                'break_end' => $breakEnd->toDateTimeString(),
+            ]);
+
+        $response = $this->get(route('user.attendance.list'));
+
+        $response->assertStatus(200);
+        $response->assertSee('前月');
+
+        $response = $this->get('/attendance/list?year=2025&month=12');
+
+        $response->assertStatus(200);
+        $previousMonth = $previousMonthData->format('Y/m');
+        $response->assertSee($previousMonth);
+        $response->assertSee($previousAttendance->work_date->isoFormat('MM/DD(ddd)'));
+        $response->assertSee($previousAttendance->clock_in->format('H:i'));
+        $response->assertSee($previousAttendance->clock_out->format('H:i'));
+        $response->assertSee('01:00');
+    }
+
+    // 翌月の勤怠データが表示されているか
+    public function test_whether_the_next_month_attendance_is_displayed()
+    {
+        Carbon::setTestNow(Carbon::create(2026, 1, 6, 8, 0, 0));
+
+        $nextMonthData = Carbon::now()->addMonth();
+        $nextAttendance = Attendance::factory()->worked($nextMonthData)->for($this->user)->create();
+
+        $clockInTime = Carbon::parse($nextAttendance->clock_in);
+        $breakStart = $clockInTime->copy()->addHour(4);
+        $breakEnd = $breakStart->copy()->addHour(1);
+        AttendanceBreak::factory()
+            ->forAttendance($nextAttendance)
+            ->create([
+                'break_start' => $breakStart->toDateTimeString(),
+                'break_end' => $breakEnd->toDateTimeString(),
+            ]);
+
+        $response = $this->get(route('user.attendance.list'));
+
+        $response->assertStatus(200);
+        $response->assertSee('翌月');
+
+        $response = $this->get('/attendance/list?year=2026&month=2');
+
+        $response->assertStatus(200);
+        $nextMonth = $nextMonthData->format('Y/m');
+        $response->assertSee($nextMonth);
+        $response->assertSee($nextAttendance->work_date->isoFormat('MM/DD(ddd)'));
+        $response->assertSee($nextAttendance->clock_in->format('H:i'));
+        $response->assertSee($nextAttendance->clock_out->format('H:i'));
+        $response->assertSee('01:00');
     }
 }
