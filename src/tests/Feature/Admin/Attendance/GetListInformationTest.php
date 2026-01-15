48e0f4f28d43e54aa07c01120815344828a5bb1e
ai2525desu <umi08062525@gmail.com>
2026-01-15 16:20:10 +0900

    管理者勤怠一覧画面情報取得テスト完成

--- src/tests/Feature/Admin/Attendance/GetListInformationTest.php
+++ src/tests/Feature/Admin/Attendance/GetListInformationTest.php
@@ -52,5 +52,93 @@ class GetListInformationTest extends AdminTestCase
     }
 
     // その日の全ユーザーの勤怠情報が正確に確認できる
-    public function 
+    public function test_to_accurately_check_the_attendance_of_all_users_on_a_given_day()
+    {
+        $response = $this->get(route('admin.attendance.list'));
+        $response->assertStatus(200);
+
+        $currentDay = Carbon::today();
+        $response->assertSee($currentDay->format('Y/m/d'));
+
+        $todayAttendances = Attendance::whereDate('work_date', $currentDay)->get();
+        $this->assertCount(2, $todayAttendances);
+
+        foreach ($todayAttendances as $attendance) {
+            $response->assertSee($attendance->user->name);
+            $response->assertSee($attendance->clock_in->format('H:i'));
+            $response->assertSee($attendance->clock_out->format('H:i'));
+            $response->assertSee($attendance->displayBreakTimeInHourFormat());
+            $response->assertSee($attendance->displayWorkingTimeInHourFormat());
+        }
+    }
+
+    // 遷移時に現在の日付が表示されているか
+    public function test_to_confirm_that_the_current_date_is_displayed_when_transitioning()
+    {
+        $response = $this->get(route('admin.attendance.list'));
+        $response->assertStatus(200);
+
+        $workDate = Carbon::today();
+        $response->assertSee($workDate->isoFormat('Y年M月D日'));
+        $response->assertSee('の勤怠');
+        $response->assertSee($workDate->format('Y/m/d'));
+    }
+
+    // 前日を押下した際に前の日付の勤怠情報が表示されているか
+    public function test_to_confirm_that_the_previous_day_attendance_information_is_displayed_when_the_previous_day_button_is_pressed()
+    {
+        $response = $this->get(route('admin.attendance.list'));
+
+        $response->assertStatus(200);
+        $response->assertSee('前日');
+
+        $currentDay = Carbon::today();
+        $previous = $currentDay->copy()->subDay()->format('Y-m-d');
+        $response = $this->get(route('admin.attendance.list', ['date' => $previous]));
+
+        $response->assertStatus(200);
+        $response->assertSee(
+            Carbon::today()->subDay()->isoFormat('Y年M月D日')
+        );
+
+        $previousAttendances = Attendance::whereDate('work_date', $previous)->get();
+        $this->assertCount(2, $previousAttendances);
+
+        foreach ($previousAttendances as $attendance) {
+            $response->assertSee($attendance->user->name);
+            $response->assertSee($attendance->clock_in->format('H:i'));
+            $response->assertSee($attendance->clock_out->format('H:i'));
+            $response->assertSee($attendance->displayBreakTimeInHourFormat());
+            $response->assertSee($attendance->displayWorkingTimeInHourFormat());
+        }
+    }
+
+    // 翌日を押下した際に次の日付の勤怠情報が表示されているか
+    public function test_to_confirm_that_the_next_day_attendance_information_is_displayed_when_the_next_day_button_is_pressed()
+    {
+        $response = $this->get(route('admin.attendance.list'));
+
+        $response->assertStatus(200);
+        $response->assertSee('翌日');
+
+        $currentDay = Carbon::today();
+        $next = $currentDay->copy()->addDay()->format('Y-m-d');
+        $response = $this->get(route('admin.attendance.list', ['date' => $next]));
+
+        $response->assertStatus(200);
+        $response->assertSee(
+            Carbon::today()->addDay()->isoFormat('Y年M月D日')
+        );
+
+        $nextAttendances = Attendance::whereDate('work_date', $next)->get();
+        $this->assertCount(2, $nextAttendances);
+
+        foreach ($nextAttendances as $attendance) {
+            $response->assertSee($attendance->user->name);
+            $response->assertSee($attendance->clock_in->format('H:i'));
+            $response->assertSee($attendance->clock_out->format('H:i'));
+            $response->assertSee($attendance->displayBreakTimeInHourFormat());
+            $response->assertSee($attendance->displayWorkingTimeInHourFormat());
+        }
+    }
 }
